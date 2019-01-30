20adbc255e9558041661e244cc450ef626a0f3fb
Maique Rosa <maique.rosa@gmail.com>
2019-01-29 23:07:44 -0200

    Configurando tela com a transação selecionada

--- src/app/home/balance-page/balance.component.ts
+++ src/app/home/balance-page/balance.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Page, View } from 'tns-core-modules/ui/page/page';
-import { Transaction } from '~/app/models/transaction';
-import { TransactionService } from '~/app/services/trasaction.service';
-import { Observable } from 'rxjs';
-import { ListView } from 'ui/list-view';
-import * as dialogs from 'tns-core-modules/ui/dialogs';
-import { RouterExtensions } from 'nativescript-angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Page, View } from "tns-core-modules/ui/page/page";
+import { Transaction } from "~/app/models/transaction";
+import { TransactionService } from "~/app/services/trasaction.service";
+import { Observable } from "rxjs";
+import { ListView } from "ui/list-view";
+import * as dialogs from "tns-core-modules/ui/dialogs";
+import { RouterExtensions } from "nativescript-angular/router";
 
 @Component({
   moduleId: module.id,
-  selector: 'BalancePage',
-  templateUrl: './balance.component.html',
-  styleUrls: ['./balance.component.css']
+  selector: "BalancePage",
+  templateUrl: "./balance.component.html",
+  styleUrls: ["./balance.component.css"]
 })
 export class BalancePageComponent implements OnInit {
   transactions$: Observable<Transaction[]>;
   accountBalance$: Observable<number>;
   lista: any[] = [];
-  constructor(private page: Page, private transactionService: TransactionService, private router: RouterExtensions) {}
+
+  constructor(
+    private page: Page,
+    private transactionService: TransactionService,
+    private router: RouterExtensions
+  ) {}
 
   ngOnInit() {
     this.accountBalance$ = this.transactionService.accountBalance$;
@@ -31,14 +36,14 @@ export class BalancePageComponent implements OnInit {
   cancelTransaction(index: number): void {
     dialogs
       .confirm({
-        title: 'Cancelar Transação',
-        message: 'Deseja realmente cancelar esta transação?',
-        okButtonText: 'Sim',
-        cancelButtonText: 'Cancelar'
+        title: "Cancelar Transação",
+        message: "Deseja realmente cancelar esta transação?",
+        okButtonText: "Sim",
+        cancelButtonText: "Cancelar"
       })
       .then(result => {
         if (result) {
-          dialogs.alert('Transação foi cancelada e seu saldo atualizado');
+          dialogs.alert("Transação foi cancelada e seu saldo atualizado");
         } else {
         }
       });
@@ -46,8 +51,8 @@ export class BalancePageComponent implements OnInit {
 
   getTransactionType(type: string): void {
     const types = {
-      Purchase: 'Compra',
-      Deposit: 'Depósito'
+      Purchase: "Compra",
+      Deposit: "Depósito"
     };
 
     return types[type];
@@ -55,8 +60,8 @@ export class BalancePageComponent implements OnInit {
 
   getPaymentType(payment: string) {
     const paymentTypes = {
-      Prepaid: 'Débito',
-      Credit: 'Crédito'
+      Prepaid: "Débito",
+      Credit: "Crédito"
     };
 
     return paymentTypes[payment];
@@ -68,7 +73,7 @@ export class BalancePageComponent implements OnInit {
     view.animate({ scale: { x: 1.1, y: 1.1 }, duration: 100 }).then(() => {
       view.animate({ scale: { x: 1, y: 1 }, duration: 100 });
       const transactionString = JSON.stringify(item);
-      this.router.navigate(['home/balance/detail'], {
+      this.router.navigate(["home/balance/detail"], {
         queryParams: {
           transaction: transactionString
         }
--- src/app/home/balance-page/detail-transaction/detail-transaction.component.ts
+++ src/app/home/balance-page/detail-transaction/detail-transaction.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Page } from 'tns-core-modules/ui/page/page';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Page } from "tns-core-modules/ui/page/page";
+import { ActivatedRoute } from "@angular/router";
+import { Transaction } from "~/app/models/transaction";
 
 @Component({
-  selector: 'ns-detail-transaction',
-  templateUrl: './detail-transaction.component.html',
-  styleUrls: ['./detail-transaction.component.css'],
+  selector: "ns-detail-transaction",
+  templateUrl: "./detail-transaction.component.html",
+  styleUrls: ["./detail-transaction.component.css"],
   moduleId: module.id
 })
 export class DetailTransactionComponent implements OnInit {
+  transaction: Transaction;
+
   constructor(private page: Page, private activetadRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activetadRoute.queryParams.subscribe(params => {
-     
+      this.transaction = JSON.parse(params["transaction"]);
     });
   }
 }
