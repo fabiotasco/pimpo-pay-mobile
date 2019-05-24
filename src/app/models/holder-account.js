0881de21b52cd9b2c2c894dbd223fa948daa375f
Maique Rosa <maique.rosa@gmail.com>
2019-05-24 14:40:31 -0300

    MASTER Removendo arquivos desnecessarios (.js, .css, .map)

--- src/app/models/holder-account.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaG9sZGVyLWFjY291bnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJob2xkZXItYWNjb3VudC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGludGVyZmFjZSBIb2xkZXJBY2NvdW50IHtcbiAgbnVtYmVyOiBzdHJpbmc7XG59XG4iXX0=
--- src/app/models/transaction.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Transaction = /** @class */ (function () {
-    function Transaction() {
-    }
-    return Transaction;
-}());
-exports.Transaction = Transaction;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHJhbnNhY3Rpb24uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJ0cmFuc2FjdGlvbi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBO0lBQUE7SUFVQSxDQUFDO0lBQUQsa0JBQUM7QUFBRCxDQUFDLEFBVkQsSUFVQztBQVZZLGtDQUFXIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIFRyYW5zYWN0aW9uIHtcbiAgaWQ6IG51bWJlcjtcbiAgZGF0ZTogRGF0ZTtcbiAgYW1vdW50OiBudW1iZXI7XG4gIGN1cnJlbmN5VHlwZTogc3RyaW5nO1xuICBpbnN0YWxsbWVudHM6IG51bWJlcjtcbiAgcmVhc29uQ29kZTogc3RyaW5nO1xuICB0eXBlOiBzdHJpbmc7XG4gIHBsYW5UeXBlOiBzdHJpbmc7XG4gIHN0YXR1czogc3RyaW5nO1xufVxuIl19
